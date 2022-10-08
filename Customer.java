67a2e5777c119f2d6281b0311622399bce6d2d4d
Samuele Reyes <s3910311@student.rmit.edu.au>
2022-10-08 18:46:28 +1100

    add bookEvent for customer

--- Customer.java
+++ Customer.java
@@ -1,3 +1,5 @@
+import java.util.*; 
+
 public class Customer {
     private String name = "";
     private String email = "";
@@ -18,9 +20,28 @@ public class Customer {
         return email;
     }
 
-    public void bookEvent() {
+    public Event bookEvent(Venue venue, Customer customer, Date date, int attendees, Package packageEvent) {
+        boolean isError = false;
         Event event = new Event();
         event.setEventID(0);
-        event.printEventID();
+        event.setVenue(venue);
+        event.setCustomer(customer);
+        event.setPackageUsed(packageEvent);
+        event.setAttendees(attendees);
+        event.reserveDate(date);
+        if (event.getReservedDate() == null){
+            System.out.println("Select an available date from an available venue");
+            isError = true;
+        }
+        if (packageEvent.getMaxPeopleServed() < attendees){
+            System.out.println("Input attendees less than the maximum number of people served");
+            isError = true;
+        }
+        if (isError){
+            return null;
+        }
+        else {
+            return event;
+        }
     }
 }
