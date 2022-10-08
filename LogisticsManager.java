2324455ac3937556f90b7a0eeb9e2cae0a928e97
Samuele Reyes <s3910311@student.rmit.edu.au>
2022-10-09 04:24:13 +1100

    add logistic manager

---
+++ LogisticsManager.java
@@ -0,0 +1,25 @@
+public class LogisticsManager extends EventManagementTeam {
+    
+    public void deliverAddition(Event event, Addition addition){
+        if (addition instanceof MusicBand){
+            event.setDeliverMusicBand(true);
+            System.out.println("Delivered " + event.getMusicBand().getName());
+        }
+        else if (addition instanceof SoundSystem){
+            event.setDeliverSoundSystem(true);
+            System.out.println("Delivered " + event.getSoundSystem().getType());
+        }
+        else if (addition instanceof FlowerArrange){
+            event.setDeliverFlowerDecor(true);
+            System.out.println("Delivered " + event.getFlowerArrange().getArrangementType());
+        }
+    }
+    public void orderAddition(Addition addition){
+        addition.setOrdered(true);
+        System.out.println("Ordered addition");
+    }
+
+    public void updatePricePerHead(Event event, int newPrice){
+        event.getFoodMenu().setPricePerHead(newPrice);
+    }
+}
