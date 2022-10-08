b00f7445573cd4f092bd1084bec4d6740e01b951
Samuele Reyes <s3910311@student.rmit.edu.au>
2022-10-09 00:28:38 +1100

    add communication options

--- Customer.java
+++ Customer.java
@@ -1,4 +1,6 @@
 import java.util.*; 
+import java.text.DateFormat;  
+import java.text.SimpleDateFormat;  
 
 public class Customer {
     private String name;
@@ -56,5 +58,57 @@ public class Customer {
         else {
             return event;
         }
+    }
+
+    public void requestInspection(Venue venue, Date requestDate) {
+        if (venue.getAvailableDates().contains(requestDate)) {
+            System.out.printf("%s is avaialble for inspection on %tB %<te, %<tY%n", venue.getName(), requestDate); 
+            Communication inspectionCommunication = new Communication();
+            inspectionCommunication.setType("Request Inspection");
+            DateFormat dateFormat = new SimpleDateFormat("MMMM dd, yyyy");  
+            String strDate = dateFormat.format(requestDate);  
+            inspectionCommunication.setMessageSent("Inspection date of " + venue.getName() + " on " + strDate);
+            inspectionCommunication.printMessageSent();
+            //send message to manager
+        }
+        else {
+            System.out.printf("%s is not avaialble for inspection on %tB %<te, %<tY, pick a different date", venue.getName(), requestDate); 
+        }
+    }
+
+    public void requestMenuAdd(Event event, String food) {
+        System.out.printf("Try to add %s to event%n", food); 
+        Communication menuAddCommunication = new Communication();
+        menuAddCommunication.setType("Request Add for Menu");
+        menuAddCommunication.setMessageSent("Add "+ food +" to menu of eventID = " + event.getEventID());
+        menuAddCommunication.printMessageSent();
+        //send message to manager
+    }
+    
+    public void requestMenuRemove(Event event, String food) {
+        System.out.printf("Try to remove %s from event%n", food); 
+        Communication menuRemoveCommunication = new Communication();
+        menuRemoveCommunication.setType("Request Remove for Menu");
+        menuRemoveCommunication.setMessageSent("Remove "+ food +" from menu of eventID = " + event.getEventID());
+        menuRemoveCommunication.printMessageSent();
+        //send message to manager
+    }
+
+    public void askQuery(String string){
+        System.out.println("Try to send a query"); 
+        Communication queryCommunication = new Communication();
+        queryCommunication.setType("Query");
+        queryCommunication.setMessageSent(string);
+        queryCommunication.printMessageSent();
+        //send message to manager
+    }
+
+    public void sendComplaint(String string){
+        System.out.println("Try to send a complaint"); 
+        Communication compaintCommunication = new Communication();
+        compaintCommunication.setType("Complaint");
+        compaintCommunication.setMessageSent(string);
+        compaintCommunication.printMessageSent();
+        //send message to manager
     }
 }
