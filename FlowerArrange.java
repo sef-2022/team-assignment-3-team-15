18a32c2c5228b1bcd9837a5599720b11afb62b3f
Samuele Reyes <s3910311@student.rmit.edu.au>
2022-10-08 21:53:15 +1100

    create flower arrange

---
+++ FlowerArrange.java
@@ -0,0 +1,16 @@
+public class FlowerArrange extends Addition{
+    private String arrangementType;
+  
+    public void setArrangementType(String arrangementType) {
+        this.arrangementType = arrangementType;
+    }
+
+    public String getArrangementType() {
+        return arrangementType;
+    }
+    
+    public void printSoundSysSummary() {
+        System.out.printf("Flower arrangement type: %s%n", arrangementType);
+        System.out.printf("Flower arrangement price: %d%n%n", price);
+    }
+}
