9068ab20fba645d54093f0ed3d03599a0264a2ea
Samuele Reyes <s3910311@student.rmit.edu.au>
2022-10-08 21:52:47 +1100

    initialise additions

--- Customer.java
+++ Customer.java
@@ -20,7 +20,7 @@ public class Customer {
         return email;
     }
 
-    public Event bookEvent(Venue venue, Customer customer, Date date, int attendees, Package packageEvent) {
+    public Event bookEvent(Venue venue, Customer customer, Date date, int attendees, Package packageEvent, boolean addMusicBand, boolean addSoundSys, boolean addFlowerArrange) {
         boolean isError = false;
         Event event = new Event();
         event.setEventID(0);
@@ -29,6 +29,9 @@ public class Customer {
         event.setPackageUsed(packageEvent);
         event.setAttendees(attendees);
         event.reserveDate(date);
+        event.setIncludesMusicBand(addMusicBand);
+        event.setIncludesSoundSystem(addSoundSys);
+        event.setIncludesFlowerDecor(addFlowerArrange);
         if (event.getReservedDate() == null){
             System.out.println("Select an available date from an available venue");
             isError = true;
