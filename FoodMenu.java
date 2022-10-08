86d9c43491d47b3ec012db76d16b4e1c7b53f3a2
Samuele Reyes <s3910311@student.rmit.edu.au>
2022-10-08 22:48:04 +1100

    add menu name and price

--- FoodMenu.java
+++ FoodMenu.java
@@ -1,7 +1,9 @@
 import java.util.*; 
 
 public class FoodMenu {
-    List<String> menu = new ArrayList<String>();
+    private List<String> menu = new ArrayList<String>();
+    private String name;
+    private int pricePerHead;
 
     public void addFoodMenu(String food) {
         menu.add(food);
@@ -13,5 +15,34 @@ public class FoodMenu {
 
     public List<String> getMenu() {
         return menu;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPricePerHead() {
+        return pricePerHead;
+    }
+
+    public void setPricePerHead(int pricePerHead) {
+        this.pricePerHead = pricePerHead;
+    }
+
+    public void printMenu() {
+        System.out.printf("Menu for %s:%n",name);
+        for (String string : menu) {
+            System.out.println(string);
+        }
+        System.out.println();
+    }
+
+    public void printMenuSummary() {
+        System.out.printf("%s Menu price per head: %s%n", name ,pricePerHead);
+        printMenu();
     }
 }
